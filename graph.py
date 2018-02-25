384cded6a944d67a2d203d157694ac4bb3c9d672
Стефан <stefan.zlatinov@gmail.com>
2018-02-26 00:14:09 +0100

    Додаден алгоритамот на Djikstra

--- graph.py
+++ graph.py
@@ -48,3 +48,52 @@ class Graph(object):
 
     def __str__(self):
         return 'Vertices: {}\nEdges: {}'.format(self.vertices(), self.edges())
+
+
+class WeightedGraph(object):
+    def __init__(self, graph_dict=defaultdict(dict)):
+        self.graph_dict = graph_dict
+
+    def vertices(self):
+        return list(self.graph_dict.keys())
+
+    def edges(self):
+        edges = []
+        for vertex in self.graph_dict:
+            for neighbour, weight in self.graph_dict[vertex].items():
+                edges.append((vertex, neighbour, weight))
+        return edges
+
+    def add_vertex(self, vertex):
+        if vertex not in self.graph_dict:
+            self.graph_dict[vertex] = {}
+
+    def add_edge(self, edge, weight, add_reversed=True):
+        vertex1, vertex2 = edge
+        self.graph_dict[vertex1][vertex2] = weight
+        if add_reversed:
+            self.graph_dict[vertex2][vertex1] = weight
+
+    def remove_vertex(self, vertex_to_remove):
+        del self.graph_dict[vertex_to_remove]
+        for vertex in self.vertices():
+            if vertex_to_remove in self.graph_dict[vertex]:
+                del self.graph_dict[vertex][vertex_to_remove]
+
+    def remove_edge(self, edge_to_remove, remove_reversed=True):
+        vertex1, vertex2 = edge_to_remove
+        if vertex2 in self.graph_dict[vertex1]:
+            del self.graph_dict[vertex1][vertex2]
+        if remove_reversed:
+            if vertex1 in self.graph_dict[vertex2]:
+                del self.graph_dict[vertex2][vertex1]
+
+    def isolated_verices(self):
+        isolated_vertices = []
+        for vertex in self.graph_dict:
+            if not self.graph_dict[vertex]:
+                isolated_vertices.append(vertex)
+        return isolated_vertices
+
+    def __str__(self):
+        return 'Vertices: {}\nEdges: {}'.format(self.vertices(), self.edges())
