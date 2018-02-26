bdeff70420f906fef26fcc2a98ca966fc7da5177
Стефан <stefan.zlatinov@gmail.com>
2018-02-26 15:55:20 +0100

    Додаден дел од решението

---
+++ Лабораториски вежби/Water.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+
+def transfer_to_other_glass(source, sink, sink_capacity):
+    # Вашиот код тука
+    pass
+
+
+def expand_state(state):
+    capacities = (5, 7, 4)
+    glass_0, glass_1, glass_2 = state
+
+    new_states = []
+
+    # Вашиот код тука
+
+    return new_states
+
+
+def search_path():
+    water_level = (5, 8, 10)
+    initial_state = water_level
+    goal_state = 1
+
+    states_queue = deque([[initial_state]])
+    while states_queue:
+        states_list = states_queue.popleft()
+        state_to_expand = states_list[-1]
+        for next_state in expand_state(state_to_expand):
+            if next_state not in states_list:
+                if next_state[0] == goal_state:
+                    return states_list + [next_state]
+                states_queue.append(states_list + [next_state])
+
+
+def visualise_path(path):
+    for states in zip(path, path[1:]):
+        old_state, new_state = states
+        print()
+        print(old_state)
+        print(tuple(map(lambda x, y: x - y, new_state, old_state)), 'change')
+        print(new_state)
+
+
+def main():
+    path = search_path()
+    visualise_path(path)
+
+
+main()
